6e7d0903ff0a99f46cd6e33d6728bfe1b0cad881
Richard <andresmarles9106@gmial.com>
2024-03-28 09:38:50 +0100

    adding css and js bootstrap min file in statcis and remove cdn

--- mindwell/settings.py
+++ mindwell/settings.py
@@ -36,7 +36,7 @@ import cloudinary.api
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
