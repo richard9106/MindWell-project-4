119d4f3fa813086e041a619bbcffadbfec94178f
RichardM <114283158+richard9106@users.noreply.github.com>
2025-09-30 09:54:39 +0200

    Update settings.py

--- mindwell/settings.py
+++ mindwell/settings.py
@@ -36,7 +36,7 @@ import cloudinary.api
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '.gitpod.io']
 
